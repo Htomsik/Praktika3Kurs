bd35383058b4e9f17b160913a30fd3589474f2d7
Константин Несутулов <186141@edu.fa.ru>
2021-05-15 16:53:57 +0300

    Добавьте файлы проекта.

---
+++ SwitchingViewsMVVM/Commands/Base/BaseCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SwitchingViewsMVVM.Commands
+{
+    internal abstract class BaseCommand:ICommand
+    {
+        public event EventHandler CanExecuteChanged //впф автоматически генерирует это событие у всех команд когда что то происходит
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+
+        }
+
+        public abstract bool CanExecute(object parameter); //ложь-выполнить нельзя, элемент отключается автоматически
+
+        public abstract void Execute(object parameter); //то что должно быть выполнено
+    }
+}
---
+++ SwitchingViewsMVVM/Commands/UpdateViewCommand.cs
@@ -0,0 +1,34 @@
+﻿using SwitchingViewsMVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SwitchingViewsMVVM.Commands
+{
+    internal class UpdateViewCommand : BaseCommand
+    {
+        private MainViewModel viewModel;
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public override bool CanExecute(object parameter) => true;
+      
+
+        public override void Execute(object parameter)
+        {
+            if(parameter.ToString() == "_Pr1")
+            {
+                viewModel.SelectedViewModel = new _Pr1ViewModel();
+            }
+            else if(parameter.ToString() == "_Pr2")
+            {
+                viewModel.SelectedViewModel = new _Pr2ViewModel();
+            }
+        }
+    }
+}
